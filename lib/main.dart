091792e4045def4a168a542db0483c74fe8ed908
SincerelyAlex <sincerelyalexhoang@gmail.com>
2026-09-16 14:46:27 -0700

    Added list of starter assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,40 @@
 import 'package:flutter/material.dart';
 
+class Artwork {
+  final String title;
+  final String artist;
+  final String year;
+  final String imagePath;
+  
+  Artwork({
+    required this.title,
+    required this.artist,
+    required this.year,
+    required this.imagePath,
+  });
+}
+
+List<Artwork> artworks = [
+  Artwork(
+    title: 'Starry Night',
+    artist: 'Vincent van Gogh',
+    year: '1889',
+    imagePath: 'assets/images/artwork/starry_night.jpg',
+  ),
+  Artwork(
+    title: 'Mona Lisa',
+    artist: 'Leonardo da Vinci',
+    year: '1503',
+    imagePath: 'assets/images/artwork/mona_lisa.jpg',
+  ),
+  Artwork(
+    title: 'The Scream',
+    artist: 'Edvard Munch',
+    year: '1893',
+    imagePath: 'assets/images/artwork/the_scream.jpg',
+  ),
+];
+
 void main() {
   runApp(const MainApp());
 }
